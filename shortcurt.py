bc7463e9c180b6b035ca684418b4a6ea9cd7b644
KumarArindam <kumararindam77@gmail.com>
2018-12-17 20:11:08 +0530

    hacks for kaggle

---
+++ shortcurt.py
@@ -0,0 +1,54 @@
+from pandas.api.types import is_string_dtype, is_numeric_dtype
+from sklearn.ensemble import RandomForestRegressor
+
+def proc_df(df, y_fld=None, skip_flds=None, ignore_flds=None, do_scale=False, na_dict=None,
+            preproc_fn=None, max_n_cat=None, subset=None, mapper=None):
+    if not ignore_flds: ignore_flds=[]
+    if not skip_flds: skip_flds=[]
+    if subset: df = get_sample(df,subset)
+    else: df = df.copy()
+    ignored_flds = df.loc[:, ignore_flds]
+    df.drop(ignore_flds, axis=1, inplace=True)
+    if preproc_fn: preproc_fn(df)
+    if y_fld is None: y = None
+    else:
+        if not is_numeric_dtype(df[y_fld]): df[y_fld] = df[y_fld].cat.codes
+        y = df[y_fld].values
+        skip_flds += [y_fld]
+    df.drop(skip_flds, axis=1, inplace=True)
+
+    if na_dict is None: na_dict = {}
+    else: na_dict = na_dict.copy()
+    na_dict_initial = na_dict.copy()
+    for n,c in df.items(): na_dict = fix_missing(df, c, n, na_dict)
+    if len(na_dict_initial.keys()) > 0:
+        df.drop([a + '_na' for a in list(set(na_dict.keys()) - set(na_dict_initial.keys()))], axis=1, inplace=True)
+    if do_scale: mapper = scale_vars(df, mapper)
+    for n,c in df.items(): numericalize(df, c, n, max_n_cat)
+    df = pd.get_dummies(df, dummy_na=True)
+    df = pd.concat([ignored_flds, df], axis=1)
+    res = [df, y, na_dict]
+    if do_scale: res = res + [mapper]
+    return res
+
+
+def fix_missing(df, col, name, na_dict):
+if is_numeric_dtype(col):
+    if pd.isnull(col).sum() or (name in na_dict):
+        df[name+'_na'] = pd.isnull(col)
+        filler = na_dict[name] if name in na_dict else col.median()
+        df[name] = col.fillna(filler)
+        na_dict[name] = filler
+return na_dict
+
+def numericalize(df, col, name, max_n_cat):
+    if not is_numeric_dtype(col) and ( max_n_cat is None or len(col.cat.categories)>max_n_cat):
+        df[name] = col.cat.codes+1
+
+def split_vals(a,n): return a[:n].copy(), a[n:].copy()
+
+def print_score(m):
+    res = [mae(m.predict(X_train), y_train), mae(m.predict(X_valid), y_valid),
+                m.score(X_train, y_train), m.score(X_valid, y_valid)]
+    if hasattr(m, 'oob_score_'): res.append(m.oob_score_)
+    print(res)
