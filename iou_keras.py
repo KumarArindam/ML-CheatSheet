73867c224845e58fe63c74657e082dbf14880f70
KumarArindam <kumararindam77@gmail.com>
2019-02-01 17:03:21 +0530

    IOU object detection

---
+++ iou_keras.py
@@ -0,0 +1,37 @@
+## implementation in keras
+
+def iou(y_true,y_pred,smooth=1):
+	intersection = K.sum(K.abs(y_true*y_pred),axis=-1)
+	union = K.sum(y_true,-1)+K.sum(y_pred,-1)-intersection
+
+	return (intersection+smooth)/(union+smooth)
+
+def iou_coef_loss(y_true,y_pred):
+	return -iou(y_true,y_pred)
+
+
+## implementation in torch
+def bbox_iou(box1, box2):
+    """
+    Returns the IoU of two bounding boxes 
+    """
+    #Get the coordinates of bounding boxes
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+    
+    #get the corrdinates of the intersection rectangle
+    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
+    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
+    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
+    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
+    
+    #Intersection area
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+
+    #Union Area
+    b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
+    
+    iou = inter_area / (b1_area + b2_area - inter_area)
+    
+    return iou
